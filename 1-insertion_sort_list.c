1060d5e563ffb4fe20e62f9bdfa7e77c22126b4c
FerchichiXIII <anasferchichi22@gmail.com>
2022-06-12 22:33:12 +0100

    task 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+}
