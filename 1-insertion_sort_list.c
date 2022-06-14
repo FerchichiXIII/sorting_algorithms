d4f385ca3cd95676976ba1603e1fa1b3ddd8436c
FerchichiXIII <anasferchichi22@gmail.com>
2022-06-14 22:11:33 +0100

    task 1$

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,11 +1,39 @@
 #include "sort.h"
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * @array: The array to be sorted
- * @size: Number of elements in @array
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * @list: The list to be sorted
  * Return: Nothing
+ *
  */
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *i, *j, *key;
+
+	if (list == NULL || list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		while (j->prev != NULL && j->n < j->prev->n)
+		{
+			key = j->prev->prev;
+			if (j->prev->prev != NULL)
+				j->prev->prev->next = j;
+			j->prev->prev = j;
+			j->prev->next = j->next;
+			if (j->next != NULL)
+				j->next->prev = j->prev;
+			j->next = j->prev;
+			j->prev = key;
+			if (j->prev == NULL)
+				list = j;
+			print_list(list);
+		}
+		i = i->next;
+	}
 }
