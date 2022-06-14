b588e2c7b9b13cf45c16de8eae0b50733858057d
FerchichiXIII <anasferchichi22@gmail.com>
2022-06-14 22:13:40 +0100

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,7 @@
 #include "sort.h"
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list -
+ * Sorts a doubly linked list of integers in ascending order
  * @list: The list to be sorted
  * Return: Nothing
  *
